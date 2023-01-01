2d50a0c466f4dc02c07cfe3238604bb4220874fb
DoodMac <48062579+jbird84@users.noreply.github.com>
2023-01-01 09:07:39 -0600

    fixed bug with heart events.

--- StardewValleyWiki/Screens/CharacterDetailsViewController.swift
+++ StardewValleyWiki/Screens/CharacterDetailsViewController.swift
@@ -13,8 +13,23 @@ class CharacterDetailsViewController: UIViewController {
     
     let characterDetailsImageView = UIImageView()
     var character: Character = Character(name: "", schedule: [], birthday: "", gifts: [], heart_events: [])
-    var filteredHearts: [HeartEvent] = []
     var characterDetailsImage = ""
+
+    //Gifts
+    let giftsStackView = UIStackView()
+    let loveStackView = UIStackView()
+    let loveLabel = UILabel()
+    let loveLabelDetails = UILabel()
+    let likeStackView = UIStackView()
+    let likeLabel = UILabel()
+    let likeLabelDetails = UILabel()
+    let neutralStackView = UIStackView()
+    let neutralLabel = UILabel()
+    let neutralLabelDetails = UILabel()
+    let hateStackView = UIStackView()
+    let hateLabel = UILabel()
+    let hateLabelDetails = UILabel()
+    
     
     //Heart_Events
     let heartStackView = UIStackView()
@@ -35,20 +50,24 @@ class CharacterDetailsViewController: UIViewController {
     let heart12 = UILabel()
     let heart13 = UILabel()
     let heart14 = UILabel()
+
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         setupcharacterDetailsImageView()
+        detailView.backgroundColor = .clear
         
     }
     
     
     
     @IBAction func birthdayButtonTapped(_ sender: Any) {
+        view.backgroundColor = UIColor(named: "detailViewBK")
         setupcharacterDetailsImageView()
         characterDetailsImageView.isHidden = false
         heartStackView.isHidden = true
+        giftsStackView.isHidden = true
     }
     
     
@@ -56,12 +75,19 @@ class CharacterDetailsViewController: UIViewController {
     }
     
     @IBAction func giftsButtonTapped(_ sender: Any) {
+        characterDetailsImageView.isHidden = true
+        heartStackView.isHidden = true
+        giftsStackView.isHidden = false
+        view.backgroundColor = UIColor(named: "giftViewBK")
+        setupGifts()
     }
     
     
     @IBAction func heartsButtonTapped(_ sender: Any) {
         characterDetailsImageView.isHidden = true
         heartStackView.isHidden = false
+        giftsStackView.isHidden = true
+        view.backgroundColor = UIColor(named: "heartViewBK")
         getHeartEvents()
         setupHeartEventStackView()
     }
@@ -73,6 +99,61 @@ class CharacterDetailsViewController: UIViewController {
         characterDetailsImageView.center = detailView.center
         characterDetailsImageView.image = image
         characterDetailsImageView.contentMode = .scaleAspectFill
+    }
+    
+    private func setupGifts() {
+        let giftLabels = [loveLabel, likeLabel, neutralLabel, hateLabel]
+        let giftDetailLabels = [loveLabelDetails, likeLabelDetails, neutralLabelDetails, hateLabelDetails]
+        let stackviews = [loveStackView, likeStackView, neutralStackView, hateStackView]
+        
+        for details in giftDetailLabels {
+            details.numberOfLines = 0
+        }
+        
+        for gift in giftLabels {
+            gift.numberOfLines = 1
+            gift.font = .systemFont(ofSize: 30, weight: .bold)
+            gift.textColor = UIColor(named: "giftLabelColor")
+        }
+        for gifts in character.gifts {
+            loveLabelDetails.text = gifts.love
+            likeLabelDetails.text = gifts.like
+            neutralLabelDetails.text = gifts.neutral
+            hateLabelDetails.text = gifts.hate
+        }
+        
+        loveLabel.text = "Gifts I Love"
+        likeLabel.text = "Gifts I Like"
+        neutralLabel.text = "Gifts I Am Neutral About"
+        hateLabel.text = "Gifts I Hate"
+        
+        loveStackView.addArrangedSubview(loveLabel)
+        loveStackView.addArrangedSubview(loveLabelDetails)
+        likeStackView.addArrangedSubview(likeLabel)
+        likeStackView.addArrangedSubview(likeLabelDetails)
+        neutralStackView.addArrangedSubview(neutralLabel)
+        neutralStackView.addArrangedSubview(neutralLabelDetails)
+        hateStackView.addArrangedSubview(hateLabel)
+        hateStackView.addArrangedSubview(hateLabelDetails)
+        giftsStackView.frame = detailView.bounds
+        detailView.addSubview(giftsStackView)
+        
+        for stackview in stackviews {
+            stackview.spacing = 5
+            stackview.axis = .vertical
+            stackview.alignment = .leading
+            stackview.distribution = .fillProportionally
+        }
+        
+        giftsStackView.addArrangedSubview(loveStackView)
+        giftsStackView.addArrangedSubview(likeStackView)
+        giftsStackView.addArrangedSubview(neutralStackView)
+        giftsStackView.addArrangedSubview(hateStackView)
+        giftsStackView.spacing = 10
+        giftsStackView.axis = .vertical
+        giftsStackView.alignment = .leading
+        giftsStackView.distribution = .fillProportionally
+        giftsStackView.anchor(top: detailView.topAnchor, leading: detailView.leadingAnchor, trailing: detailView.trailingAnchor, paddingTop: 20, paddingLeading: 16, paddingTrailing: 16)
     }
     
     private func setupHeartEventStackView() {
@@ -100,30 +181,29 @@ class CharacterDetailsViewController: UIViewController {
         heartStackView.axis = .vertical
         heartStackView.alignment = .leading
         heartStackView.distribution = .fillProportionally
-        
         heartStackView.anchor(top: detailView.topAnchor, leading: detailView.leadingAnchor, trailing: detailView.trailingAnchor, paddingTop: 20, paddingLeading: 16, paddingTrailing: 16)
     }
     
     private func getHeartEvents() {
-        filteredHearts = []
         for heart in character.heart_events {
-            heart2.text = "2 hearts: \(heart.twoHearts)"
-            heart3.text = "3 hearts: \(heart.threeHearts)"
-            heart3a.text = "3 hearts 2: \(heart.threeHearts2)"
-            heart4.text = "4 hearts: \(heart.fourHearts)"
-            heart5.text = "5 hearts: \(heart.fiveHearts)"
-            heart6.text = "6 hearts: \(heart.sixHearts)"
-            heart6a.text = "6 hearts 2 \(heart.sixHearts2)"
-            heart7.text = "7 hearts: \(heart.sevenHearts)"
-            heart7a.text = "7 hearts 2: \(heart.sevenHearts2)"
-            heart7b.text = "7 hearts 3: \(heart.sevenHearts3)"
-            heart8.text = "8 hearts: \(heart.eightHearts)"
-            heart9.text = "9 hearts:\(heart.nineHearts)"
-            heart10.text = "10 hearts: \(heart.tenHearts)"
-            heart11.text = "11 hearts: \(heart.elevenHearts)"
-            heart12.text = "12 hearts: \(heart.twelveHearts)"
-            heart13.text = "13 hearts: \(heart.thirteenHearts)"
-            heart14.text = "14 hearts:\(heart.fourteenHearts)"
+
+            applyBoldFormatting(to: heart2, text: "2 hearts: \n\(heart.twoHearts)", boldRange: NSRange(location: 0, length: 10))
+            applyBoldFormatting(to: heart3, text: "3 hearts: \n\(heart.threeHearts)", boldRange: NSRange(location: 0, length: 10))
+            applyBoldFormatting(to: heart3a, text: "3 heartA: \n\(heart.threeHearts2)", boldRange: NSRange(location: 0, length: 10))
+            applyBoldFormatting(to: heart4, text: "4 hearts: \n\(heart.fourHearts)", boldRange: NSRange(location: 0, length: 10))
+            applyBoldFormatting(to: heart5, text: "5 hearts: \n\(heart.fiveHearts)", boldRange: NSRange(location: 0, length: 10))
+            applyBoldFormatting(to: heart6, text: "6 hearts: \n\(heart.sixHearts)", boldRange: NSRange(location: 0, length: 10))
+            applyBoldFormatting(to: heart6a, text: "6 heartA: \n\(heart.sixHearts2)", boldRange: NSRange(location: 0, length: 10))
+            applyBoldFormatting(to: heart7, text: "7 hearts: \n\(heart.sevenHearts)", boldRange: NSRange(location: 0, length: 10))
+            applyBoldFormatting(to: heart7a, text: "7 heartA: \n\(heart.sevenHearts2)", boldRange: NSRange(location: 0, length: 10))
+            applyBoldFormatting(to: heart7b, text: "7 heartB: \n\(heart.sevenHearts3)", boldRange: NSRange(location: 0, length: 10))
+            applyBoldFormatting(to: heart8, text: "8 hearts: \n\(heart.eightHearts)", boldRange: NSRange(location: 0, length: 10))
+            applyBoldFormatting(to: heart9, text: "9 hearts: \n\(heart.nineHearts)", boldRange: NSRange(location: 0, length: 10))
+            applyBoldFormatting(to: heart10, text: "10 hearts: \n\(heart.tenHearts)", boldRange: NSRange(location: 0, length: 10))
+            applyBoldFormatting(to: heart11, text: "11 hearts: \n\(heart.elevenHearts)", boldRange: NSRange(location: 0, length: 10))
+            applyBoldFormatting(to: heart12, text: "12 hearts: \n\(heart.twelveHearts)", boldRange: NSRange(location: 0, length: 10))
+            applyBoldFormatting(to: heart13, text: "13 hearts: \n\(heart.thirteenHearts)", boldRange: NSRange(location: 0, length: 10))
+            applyBoldFormatting(to: heart14, text: "14 hearts: \n\(heart.fourteenHearts)", boldRange: NSRange(location: 0, length: 10))
         }
         removeEmptyHeartEvents()
     }
@@ -140,5 +220,11 @@ class CharacterDetailsViewController: UIViewController {
                 heart.isHidden = false
             }
         }
+    }
+    
+    func applyBoldFormatting(to label: UILabel, text: String, boldRange: NSRange) {
+        let attributedText = NSMutableAttributedString(string: text)
+        attributedText.addAttribute(.font, value: UIFont.boldSystemFont(ofSize: 22), range: boldRange)
+        label.attributedText = attributedText
     }
 }
